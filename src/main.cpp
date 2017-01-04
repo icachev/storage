26beb73c36a60716ca739f0db74371fc629b3f45
Ivaylo <ivaylo_66@abv.bg>
2017-01-04 19:33:33 +0200

    Init commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<stdlib.h>
+using namespace std;
+
+#include "Product.h"
+
+int main(){
+    Product a;
+    a.input();
+    cout << endl; system("pause"); system("CLS");
+    a.output();
+    cout << endl; system("pause");
+}
